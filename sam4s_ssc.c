58b961c4f5bb4795187bf78290765d88c1068c28
Christian Vogel <vogelchr@vogel.cx>
2018-08-25 10:56:38 +0200

    sam4s_ssc: Add realignment into interrupt handler.
    That way we save one useless copy between input buffer,
    realignment buffer, output buffer.

--- sam4s_ssc.c
+++ sam4s_ssc.c
@@ -24,17 +24,79 @@
 #include "sam4s_clock.h"
 #include "sam4s_pinmux.h"
 
-unsigned char sam4s_ssc_rxbuf[SAM4S_SSC_BUF_NFRAMES*SAM4S_SSC_FRAME_BYTES];
-int sam4s_ssc_rx_last_frame;
-static int sam4s_ssc_rx_curr_frame;
+/* ==== TRANSMITTING ==== */
 
-unsigned char sam4s_ssc_txbuf[SAM4S_SSC_BUF_NFRAMES*SAM4S_SSC_FRAME_BYTES];
-int sam4s_ssc_tx_last_frame;
-static int sam4s_ssc_tx_curr_frame;
+/* externally visible buffer for data that needs to be transmitted */
+unsigned char sam4s_ssc_txbuf[SAM4S_SSC_BUF_TX_NFRAMES*SAM4S_SSC_FRAME_BYTES];
+static int sam4s_ssc_tx_curr_frame; /* internally used: currently active frame */
+int sam4s_ssc_tx_last_frame;        /* externally used: last successfully sent frame */
 
-/* statistics for debugging */
-unsigned long sam4s_ssc_tx_irq_ctr;
-unsigned long sam4s_ssc_rx_irq_ctr;
+
+/* ==== RECEIVING ==== */
+
+#define SAM4S_SSC_BUF_RXRAW_NFRAMES 2   /* we only use 2 buffers for non-aligned frame data */
+
+/* internal buffer to receive raw, unaligned frames */
+static unsigned char sam4s_ssc_rxraw_buf[SAM4S_SSC_BUF_RXRAW_NFRAMES*SAM4S_SSC_FRAME_BYTES];
+static int sam4s_ssc_rxraw_curr_frame;
+
+/* externally visible buffer for received realigned data */
+unsigned char sam4s_ssc_rx_buf[SAM4S_SSC_BUF_RX_NFRAMES * SAM4S_SSC_FRAME_BYTES];
+int sam4s_ssc_rx_last_frame;        /* ext. used: last sucecssfully read & realigned */
+
+/* internally used... */
+static unsigned char *sam4s_ssc_rx_writep; /* next char to be written */
+static unsigned char *sam4s_ssc_rx_buf_end = sam4s_ssc_rx_buf + sizeof(sam4s_ssc_rx_buf);
+static uint32_t sam4s_ssc_rx_shiftreg;     /* carry fractional octets over... */
+static int sam4s_ssc_rx_align;             /* number of bits aligned data trains input data */
+
+/* ==== STATISTICS ==== */
+
+unsigned long sam4s_ssc_rx_irq_ctr; /* statistics */
+unsigned long sam4s_ssc_tx_irq_ctr; /* statistics */
+
+void
+sam4s_ssc_realign_reset(int align)
+{
+	int i;
+
+	align = align % SAM4S_SSC_FRAME_BYTES;
+
+	__disable_irq();
+
+	sam4s_ssc_rx_align = align;
+	sam4s_ssc_rx_last_frame = -1;
+	sam4s_ssc_rx_writep = sam4s_ssc_rx_buf + (align / 8);
+	sam4s_ssc_rx_shiftreg = 0;
+
+	for (i=0; i<SAM4S_SSC_FRAME_BYTES; i++)
+		sam4s_ssc_rx_buf[i] = 0;
+
+	__enable_irq();
+}
+
+/* to be called by the ISR */
+static void
+sam4s_ssc_realign_frame(unsigned char *readp)
+{
+	int i;
+	unsigned char c;
+	int align_fract = sam4s_ssc_rx_align % 8;
+	unsigned char *writep = sam4s_ssc_rx_writep;
+	unsigned char shiftreg = sam4s_ssc_rx_shiftreg;
+
+	for (i=0; i<SAM4S_SSC_FRAME_BYTES; i++) {
+		c = *readp++;
+		*writep++ = ( c >> align_fract ) | shiftreg;
+		shiftreg = c << (8-align_fract);
+		if (writep == sam4s_ssc_rx_buf_end) /* wraparound */
+			writep = sam4s_ssc_rx_buf;
+	}
+
+	sam4s_ssc_rx_shiftreg = shiftreg;
+	sam4s_ssc_rx_writep = writep;
+	sam4s_ssc_rx_last_frame = (sam4s_ssc_rx_last_frame + 1) % SAM4S_SSC_BUF_RX_NFRAMES;
+}
 
 void
 SSC_Handler()
@@ -43,18 +105,17 @@ SSC_Handler()
 	/* "current" rx buffer has finished receiving, but we have to submit
 	   the one after the next (rxbuf_submit) to keep the queue full */
 	if (SSC->SSC_SR & SSC_SR_ENDRX) {
-		int cp = sam4s_ssc_rx_curr_frame;
+		int cp = sam4s_ssc_rxraw_curr_frame;
 
-		/* this is the period that just finished being received */
-		sam4s_ssc_rx_last_frame = cp; /* last frame is for consumer! */
+		sam4s_ssc_realign_frame(&sam4s_ssc_rxraw_buf[cp]);
 
 		/* this is the period that is currently being received */
-		cp = (cp + 1) % SAM4S_SSC_BUF_NFRAMES;
-		sam4s_ssc_rx_curr_frame = cp;
+		cp = (cp + 1) % SAM4S_SSC_BUF_RXRAW_NFRAMES;
+		sam4s_ssc_rxraw_curr_frame = cp;
 
 		/* this is the next period that will be received */
-		cp = ( cp + 1 ) % SAM4S_SSC_BUF_NFRAMES;
-		PDC_SSC->PERIPH_RNPR = (uint32_t) &sam4s_ssc_rxbuf[cp * SAM4S_SSC_FRAME_BYTES];
+		cp = ( cp + 1 ) % SAM4S_SSC_BUF_RXRAW_NFRAMES;
+		PDC_SSC->PERIPH_RNPR = (uint32_t) &sam4s_ssc_rxraw_buf[cp * SAM4S_SSC_FRAME_BYTES];
 		PDC_SSC->PERIPH_RNCR = SAM4S_SSC_FRAME_BYTES;
 
 		/* debug */
@@ -67,10 +128,10 @@ SSC_Handler()
 	
 		sam4s_ssc_tx_last_frame = cp;
 
-		cp = (cp + 1) % SAM4S_SSC_BUF_NFRAMES;
+		cp = (cp + 1) % SAM4S_SSC_BUF_TX_NFRAMES;
 		sam4s_ssc_tx_curr_frame = cp;
 
-		cp = ( cp + 1 ) % SAM4S_SSC_BUF_NFRAMES;
+		cp = ( cp + 1 ) % SAM4S_SSC_BUF_TX_NFRAMES;
 		PDC_SSC->PERIPH_TNPR = (uint32_t) &sam4s_ssc_txbuf[cp * SAM4S_SSC_FRAME_BYTES];
 		PDC_SSC->PERIPH_TNCR = SAM4S_SSC_FRAME_BYTES;
 
@@ -112,16 +173,16 @@ sam4s_ssc_init()
 	SSC->SSC_CR = SSC_CR_TXEN | SSC_CR_RXEN;
 
 	sam4s_ssc_tx_last_frame = -1;
-	sam4s_ssc_rx_last_frame = -1;
-
 	sam4s_ssc_tx_curr_frame = 0;
-	sam4s_ssc_rx_curr_frame = 0;
+
+	sam4s_ssc_rxraw_curr_frame = 0;
+	sam4s_ssc_realign_reset(0);
 
 	/* enable DMA for SSC, receive and transmit...
 	  first next pointer register, then pointer register and counters */
-	PDC_SSC->PERIPH_RNPR = (uint32_t)&sam4s_ssc_rxbuf[SAM4S_SSC_FRAME_BYTES];
+	PDC_SSC->PERIPH_RNPR = (uint32_t)&sam4s_ssc_rxraw_buf[SAM4S_SSC_FRAME_BYTES];
 	PDC_SSC->PERIPH_RNCR = SAM4S_SSC_FRAME_BYTES;
-	PDC_SSC->PERIPH_RPR = (uint32_t)&sam4s_ssc_rxbuf;
+	PDC_SSC->PERIPH_RPR = (uint32_t)&sam4s_ssc_rxraw_buf;
 	PDC_SSC->PERIPH_RCR = SAM4S_SSC_FRAME_BYTES;
 
 	PDC_SSC->PERIPH_TNPR = (uint32_t)&sam4s_ssc_txbuf[SAM4S_SSC_FRAME_BYTES];
--- sam4s_ssc.h
+++ sam4s_ssc.h
@@ -2,14 +2,22 @@
 #define SAM4S_SCC_H
 
 #define SAM4S_SSC_FRAME_BYTES 32
-#define SAM4S_SSC_BUF_NFRAMES 4
+#define SAM4S_SSC_BUF_RX_NFRAMES 2
+#define SAM4S_SSC_BUF_TX_NFRAMES 4
+
+/* 
+   after realign_reset, the next frame read will be rxbuf[0..(N-1)] and
+   rx buffer will begin with align zero-bits before received and realigned
+   frame data starts
+ */
+extern void sam4s_ssc_realign_reset(int align);
 
 extern void sam4s_ssc_init();
 
-extern unsigned char sam4s_ssc_rxbuf[SAM4S_SSC_BUF_NFRAMES*SAM4S_SSC_FRAME_BYTES];
+extern unsigned char sam4s_ssc_rxbuf[SAM4S_SSC_BUF_RX_NFRAMES*SAM4S_SSC_FRAME_BYTES];
 extern int sam4s_ssc_rx_last_frame;
 
-extern unsigned char sam4s_ssc_txbuf[SAM4S_SSC_BUF_NFRAMES*SAM4S_SSC_FRAME_BYTES];
+extern unsigned char sam4s_ssc_txbuf[SAM4S_SSC_BUF_TX_NFRAMES*SAM4S_SSC_FRAME_BYTES];
 extern int sam4s_ssc_tx_last_frame;
 
 unsigned long sam4s_ssc_tx_irq_ctr;
